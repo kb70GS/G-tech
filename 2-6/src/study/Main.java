180fc37e260dd0e10d2ad131cebbb7f94914df8f
kb70GS <kobayashi929nao@gmail.com>
2026-02-24 17:27:30 +0900

    カリキュラム追加
    backlog同様の workspace/java_curriculum 配下

---
+++ 2-6/src/study/Main.java
@@ -0,0 +1,29 @@
+package study;
+
+/**
+ * 2-6 : 課題内容
+ *
+ * 本課題では、フィールド変数、コンストラクタ、メソッドの基本的な使い方を学んでいきましょう。
+ * 課題範囲は以下のとおりです。
+ *   Main.java: 問①から問③
+ *   Study.java: 問①から問④
+ * 指定された値と変数名を守って記述してください。
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Strigクラスのhelloとstudy変数の定義
+        String hello = "Hello!";
+        String study = "Javaカリキュラム";
+
+        // ① 以下のルールに従いStudyクラスのインスタンスを生成してください。
+        // 変数名:「hj」
+
+        // ② 生成したインスタンスより、StudyクラスのprintConnectedStringメソッドを呼び出しなさい。
+
+        // ③ 生成したインスタンスより、Studyクラスのintroduceメソッドを呼び出しなさい。
+    }
+
+}
